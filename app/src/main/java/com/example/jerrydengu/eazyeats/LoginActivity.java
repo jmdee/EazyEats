04ef2922c8f429cef7dab5dfdcaa2586a171f677
Jerry Dengu <jerry.demcj@gmail.com>
2017-01-21 10:46:56 +0200

    I have created the login page  , reg page and the menu page , now working on creating classes to connect to the php so that we upload to the mysql database

--- app/src/main/java/com/example/jerrydengu/eazyeats/LoginActivity.java
+++ app/src/main/java/com/example/jerrydengu/eazyeats/LoginActivity.java
@@ -1,7 +1,12 @@
 package com.example.jerrydengu.eazyeats;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        final EditText Emaillogin =(EditText) findViewById(R.id.Emaillogin);
+        final EditText Passwordlog =  (EditText) findViewById(R.id.Passwordlog);
+        final Button Signin = (Button) findViewById(R.id.buttonsignin);
+        final TextView googlebook = (TextView) findViewById(R.id.googleBook);
+        final Button google = (Button) findViewById(R.id.google);
+        final Button facebook = (Button) findViewById(R.id.facebook);
+        final TextView donthaveaccount = (TextView) findViewById(R.id.donthaveaccount);
+        final TextView signuplogin = (TextView) findViewById(R.id.SignUpLogin);
+        signuplogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                Intent registerIntent = new Intent(LoginActivity.this , RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/jerrydengu/eazyeats/RegisterActivity.java
+++ app/src/main/java/com/example/jerrydengu/eazyeats/RegisterActivity.java
@@ -1,7 +1,12 @@
 package com.example.jerrydengu.eazyeats;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        final EditText Namereg = (EditText) findViewById(R.id.Namereg);
+        final EditText Surnamereg = (EditText) findViewById(R.id.Surnamereg );
+        final EditText Emailreg = (EditText) findViewById(R.id.Emailreg);
+        final EditText phonereg = (EditText) findViewById(R.id.phonereg );
+        final EditText Cityreg = (EditText) findViewById(R.id.Cityreg);
+        final EditText streetReg = (EditText) findViewById(R.id.streetReg);
+        //final TextView Agereg = (TextView) findViewById(R.id. );
+        final Button  Buttonreg = (Button) findViewById(R.id.Buttonreg);
+        final EditText Passwordreg = (EditText) findViewById(R.id.Passwordreg);
+        final TextView douaccountreg = (TextView) findViewById(R.id.Qnaccount);
+        final TextView Signinreg = (TextView) findViewById(R.id.Signinreg);
+        Signinreg.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent loginIntent = new Intent(RegisterActivity.this,LoginActivity.class);
+                RegisterActivity.this.startActivity(loginIntent);
+            }
+        });
     }
 }
