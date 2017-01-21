31f0c1b586926f0ec446b3eae72afc42cf5693ad
Jerry Dengu <jerry.demcj@gmail.com>
2017-01-21 12:51:54 +0200

    Registration and login complete

--- app/src/main/java/com/example/jerrydengu/eazyeats/LoginActivity.java
+++ app/src/main/java/com/example/jerrydengu/eazyeats/LoginActivity.java
@@ -1,12 +1,19 @@
 package com.example.jerrydengu.eazyeats;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -30,6 +37,39 @@ public class LoginActivity extends AppCompatActivity {
                 LoginActivity.this.startActivity(registerIntent);
 
             }
+        });
+        Signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = Emaillogin.getText().toString();
+                final String password = Passwordlog.getText().toString();
+                Response.Listener<String> resoponseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                        JSONObject jsonResponse = new JSONObject(response);
+                        boolean success = jsonResponse.getBoolean("success");
+                        if(success){
+                            String name = jsonResponse.getString("name");
+                            String surname = jsonResponse.getString("surname");
+                            Intent intent = new Intent(LoginActivity.this,MenuPageActivity.class);
+                            LoginActivity.this.startActivity(intent);
+                        }else{
+                            AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                            builder.setMessage("Login Failed")
+                                    .setNegativeButton("Retry", null)
+                                    .create()
+                                    .show();
+                        }
+                    }
+                    catch(JSONException e){
+                        e.printStackTrace();
+                    }
+                }
+                };
+                LoginRequest loginRequest = new LoginRequest(username,password,resoponseListener);
+            }
+
         });
 
 
